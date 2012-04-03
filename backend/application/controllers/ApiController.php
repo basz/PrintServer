12437ac4c66409bbbd717c0ef3f2a91da267a594
Bas Kamer <bas@bushbaby.nl>
2012-04-03 15:12:45 +0200

    Modified tmp dir cleaning

--- backend/application/controllers/ApiController.php
+++ backend/application/controllers/ApiController.php
@@ -1,10 +1,12 @@
 <?php
 
-class ApiController extends Zend_Controller_Action {
+class ApiController extends Zend_Controller_Action
+{
 
-    public function init() {
-        if (!file_exists(APPLICATION_PATH . '/configs/printers-'.APPLICATION_DOMAIN.'.ini'))
-            throw new Zend_Exception('/configs/printers-'.APPLICATION_DOMAIN.'.ini does not exists');
+    public function init()
+    {
+        if (!file_exists(APPLICATION_PATH . '/configs/printers-' . APPLICATION_DOMAIN . '.ini'))
+            throw new Zend_Exception('/configs/printers-' . APPLICATION_DOMAIN . '.ini does not exists');
 
         if (file_exists(APPLICATION_PATH . '/../data/database.sqlite') && !is_writable(APPLICATION_PATH . '/../data/database.sqlite'))
             throw new Zend_Exception('../data/database.sqlite is not writable');
@@ -25,13 +27,14 @@ class ApiController extends Zend_Controller_Action {
     /**
      * setsup database
      */
-    public function setupAction() {
+    public function setupAction()
+    {
 
         try {
             $this->_helper->viewRenderer->setNoRender();
 
             try {
-                $config = new Zend_Config_Ini( APPLICATION_PATH . '/configs/printers-'.APPLICATION_DOMAIN.'.ini', APPLICATION_ENV);
+                $config         = new Zend_Config_Ini(APPLICATION_PATH . '/configs/printers-' . APPLICATION_DOMAIN . '.ini', APPLICATION_ENV);
                 $createPrinters = $config->__isset('printers') ? $config->printers->toArray() : array();
             } catch (Zend_Config_Exception $e) {
                 $createPrinters = array();
@@ -42,14 +45,17 @@ class ApiController extends Zend_Controller_Action {
                 \Application\Model\PrintMaster::cancelJob($queued_job);
 
             // remove all files in tmp
-            foreach (new DirectoryIterator(APPLICATION_PATH . '/../data/tmp') as $file)
-                if ($file->isFile())
+            foreach (new DirectoryIterator(APPLICATION_PATH . '/../data/tmp') as $file) {
+                if ($file->isFile() && !$file->isDot() && substr($file->getFilename(), 0, 1) !== '.') {
                     @unlink($file->getRealPath());
+                }
+            }
 
             // deleting definitions
             $printers = $this->_em->getRepository('Application\Model\Entity\PrinterDefinition')->findAll();
-            foreach ($printers as $printer)
+            foreach ($printers as $printer) {
                 $this->_em->remove($printer);
+            }
 
             $cupsPrinterIds = \Application\Model\PrintMaster::getDefinedCupsPrinters();
 
@@ -76,7 +82,7 @@ class ApiController extends Zend_Controller_Action {
 
                 if (count($_printerOptions)) {
                     $_printerOptions = implode(' ', $_printerOptions);
-                    $printerOptions = $this->parsePrinterOptions($_printerOptions);
+                    $printerOptions  = $this->parsePrinterOptions($_printerOptions);
 
                     $printer->setDefaultOptions($printerOptions);
                 }
@@ -95,7 +101,8 @@ class ApiController extends Zend_Controller_Action {
     /**
      * envelopes an error
      */
-    public function envelopeAction() {
+    public function envelopeAction()
+    {
         $this->view->result = $this->getRequest()->getParam('result', false);
 
         if ($this->view->result)
@@ -107,7 +114,8 @@ class ApiController extends Zend_Controller_Action {
     /**
      * Poll for problems with any printer
      */
-    public function statusAction() {
+    public function statusAction()
+    {
         $result = \Application\Model\PrintMaster::getStatus();
 
         $status = ($result->status) ? 'OK' : 'PROBLEMATIC';
@@ -121,7 +129,7 @@ class ApiController extends Zend_Controller_Action {
         $this->view->showAlert = !$result->status;
 
 
-        $this->_forward('envelope', null, null, array('result' => true, 'response' => $response));
+        $this->_forward('envelope', null, null, array('result'   => true, 'response' => $response));
     }
 
     /**
@@ -129,17 +137,19 @@ class ApiController extends Zend_Controller_Action {
      *
      * @throws undefined printer / 1
      */
-    public function printerStatusAction() {
+    public function printerStatusAction()
+    {
         $this->view->headMeta()->appendHttpEquiv('Refresh', '10;URL=' . $this->view->url());
 
-        $this->_forward('envelope', null, null, array('result' => true, 'response' => (object) array('status' => 'NOT-YET-IMPLEMENTED')));
+        $this->_forward('envelope', null, null, array('result'   => true, 'response' => (object) array('status' => 'NOT-YET-IMPLEMENTED')));
     }
 
     /**
      * Get a list of defined printers
      */
-    public function printersAction() {
-        $this->_forward('envelope', null, null, array('result' => true, 'response' => array_values(\Application\Model\PrintMaster::getDefinedPrinters())));
+    public function printersAction()
+    {
+        $this->_forward('envelope', null, null, array('result'   => true, 'response' => array_values(\Application\Model\PrintMaster::getDefinedPrinters())));
     }
 
     /**
@@ -150,11 +160,12 @@ class ApiController extends Zend_Controller_Action {
      * @throws document to large / 5
      * @throws undefined printer / 1
      */
-    public function submitAction() {
+    public function submitAction()
+    {
         $this->_helper->contextSwitch()->setAutoJsonSerialization(false);
 
         $baseAction = Zend_Controller_Action_HelperBroker::getStaticHelper('url')->url(array('printer_id' => null), 'api_submit');
-        $form = new Application_Form_Submit(array('action' => $baseAction));
+        $form        = new Application_Form_Submit(array('action' => $baseAction));
         $form->getElement('action')->setValue($baseAction);
 
         if ($this->getRequest()->isGet()) {
@@ -165,31 +176,31 @@ class ApiController extends Zend_Controller_Action {
             $data = array_merge(array('printer_id' => $this->getRequest()->getParam('printer_id', null)), $this->getRequest()->getPost());
 
             if ($form->isValid($data)) {
-                $printer_id = $form->printer_id->getValue();
+                $printer_id   = $form->printer_id->getValue();
                 $form->document->receive();
                 $documentPath = $form->document->getFileName();
 
                 $res = \Application\Model\PrintMaster::createJob($printer_id, $documentPath);
 
                 if (is_numeric($res)) {
-                    $this->_forward('envelope', null, null, array('result' => true, 'response' => (int) $res));
+                    $this->_forward('envelope', null, null, array('result'   => true, 'response' => (int) $res));
                 } else {
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) $res));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) $res));
                 }
 
 //                return;
             } else {
                 $errors = $form->getErrors();
                 if (isset($errors['printer_id']) && in_array('isEmpty', $errors['printer_id']))
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'undefined printer', 'code' => 1)));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'undefined printer', 'code'    => 1)));
                 elseif (isset($errors['printer_id']) && in_array('notInArray', $errors['printer_id']))
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'undefined printer', 'code' => 1)));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'undefined printer', 'code'    => 1)));
                 elseif (isset($errors['document']) && in_array('fileUploadErrorNoFile', $errors['document']))
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'document missing', 'code' => 3)));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'document missing', 'code'    => 3)));
                 elseif (isset($errors['document']) && in_array('fileExtensionFalse', $errors['document']))
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'document not a PDF', 'code' => 4)));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'document not a PDF', 'code'    => 4)));
                 elseif (isset($errors['document']) && in_array('fileUploadErrorIniSize', $errors['document']))
-                    $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'document to large', 'code' => 5)));
+                    $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'document to large', 'code'    => 5)));
                 else
                     Zend_Debug::dump($errors, 'uncatched FIX ME!');
 
@@ -203,22 +214,23 @@ class ApiController extends Zend_Controller_Action {
     /**
      * @throws unknown job / 2
      */
-    public function jobStatusAction() {
-        $this->_forward('envelope', null, null, array('result' => true, 'response' => (object) array('status' => 'NOT-YET-IMPLEMENTED')));
+    public function jobStatusAction()
+    {
+        $this->_forward('envelope', null, null, array('result'   => true, 'response' => (object) array('status' => 'NOT-YET-IMPLEMENTED')));
         return;
 
         $job_id = $this->getRequest()->getParam('job_id', null);
 
         $job = $this->_em->find('Application\Model\Entity\PrinterJob', $job_id);
         if (!$job) {
-            $this->_forward('envelope', null, null, array('result' => false, 'error' => (object) array('message' => 'unknown job', 'code' => 2)));
+            $this->_forward('envelope', null, null, array('result' => false, 'error'  => (object) array('message' => 'unknown job', 'code'    => 2)));
             return;
         }
 
         $status = $job->getStatus() == 'NEW' ? 'PROBLEMATIC' : 'OK';
 
 
-        $this->_forward('envelope', null, null, array('result' => true, 'response' => (object) array('status' => $status)));
+        $this->_forward('envelope', null, null, array('result'   => true, 'response' => (object) array('status' => $status)));
     }
 
     /**
@@ -279,16 +291,17 @@ class ApiController extends Zend_Controller_Action {
 //        return $result;
 //    }
 
-    private function parsePrinterOptions($rawOptions) {
+    private function parsePrinterOptions($rawOptions)
+    {
         $options = array();
 
         do {
-            $key = '';
+            $key   = '';
             $value = '';
 
             $firstEqualSignPos = strpos($rawOptions, '=');
-            $key = trim(substr($rawOptions, 0, $firstEqualSignPos));
-            $rawOptions = trim(substr($rawOptions, $firstEqualSignPos + 1, strlen($rawOptions)));
+            $key               = trim(substr($rawOptions, 0, $firstEqualSignPos));
+            $rawOptions        = trim(substr($rawOptions, $firstEqualSignPos + 1, strlen($rawOptions)));
 
 
             // find value
@@ -296,16 +309,16 @@ class ApiController extends Zend_Controller_Action {
             $firstQuotePos = strpos($rawOptions, '\''); // values might be placed between signle quotes
 
             if ($firstQuotePos === false || $firstSpacePos < $firstQuotePos) {
-                $value = trim(substr($rawOptions, 0, $firstSpacePos));
+                $value      = trim(substr($rawOptions, 0, $firstSpacePos));
                 $rawOptions = trim(substr($rawOptions, $firstSpacePos));
             } else {
                 // remove first quote, which should be at position 0
                 $rawOptions = substr($rawOptions, 1, strlen($rawOptions));
 
                 $secondQuotePos = strpos($rawOptions, '\'');
-                $value = trim(substr($rawOptions, 0, $secondQuotePos));
+                $value          = trim(substr($rawOptions, 0, $secondQuotePos));
 
-                $rawOptions = trim(substr($rawOptions, $secondQuotePos + 1, strlen($rawOptions)));
+                $rawOptions    = trim(substr($rawOptions, $secondQuotePos + 1, strlen($rawOptions)));
             }
             if (strlen($key) && strlen($value))
                 $options[$key] = $value;
